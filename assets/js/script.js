cd2dd891de54db3668e84182eedcb3368e820382
patito <p.sotosellanes@hotmail.com>
2021-05-16 12:44:43 -0400

    add multiplication

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,25 @@ var accumulated=0;
 var firstOperation=true;
 var add=false;
 var sustract=false;
-var divide=false;
 var multiply=false;
+var divide=false;
 
 function display_numbers(number){
   document.getElementById("display").value=amount+number;
   amount=document.getElementById("display").value;
 }
 
+function clear_values(){
+  document.getElementById("display").value=0;
+  accumulated=0;
+}
+
 function addition(){
     if(sustract){
       accumulated=accumulated-parseInt(amount);
+      document.getElementById("display").value=accumulated;
+    }else if(multiply){
+      accumulated=accumulated*parseInt(amount);
       document.getElementById("display").value=accumulated;
     }else{
       accumulated=accumulated+parseInt(amount);
@@ -23,6 +31,7 @@ function addition(){
     }
   amount="";
   sustract=false;
+  multiply=false;
   add=true;
   firstOperation=false;
 }
@@ -31,6 +40,9 @@ function substraction(){
   if(!firstOperation){
     if(add){
       accumulated=accumulated+parseInt(amount);
+      document.getElementById("display").value=accumulated;
+    }else if(multiply){
+      accumulated=accumulated*parseInt(amount);
       document.getElementById("display").value=accumulated;
     }else{
       accumulated=accumulated-parseInt(amount);
@@ -42,13 +54,29 @@ function substraction(){
   }  
   amount="";
   add=false;
+  multiply=false;
   sustract=true;
 }
 
 function multiplication(){
-  accumulated=accumulated*parseInt(amount);
-  document.getElementById("display").value=accumulated;
+  if(!firstOperation){
+    if(add){
+      accumulated=accumulated+parseInt(amount);
+      document.getElementById("display").value=accumulated;
+    }else if(sustract){
+      accumulated=accumulated-parseInt(amount);
+      document.getElementById("display").value=accumulated;
+    }else{ 
+      accumulated=accumulated*parseInt(amount);
+      document.getElementById("display").value=accumulated;
+    }  
+  }else{
+    accumulated=parseInt(amount);
+    firstOperation=false;
+  }  
   amount="";
+  add=false;
+  sustract=false;
   multiply=true;
 }
 
